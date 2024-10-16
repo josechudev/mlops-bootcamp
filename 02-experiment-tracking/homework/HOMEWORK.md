7027948e6d504f12fd5c455f3e02a3628c4b7a70
José Chu <jchudb93@gmail.com>
2024-10-16 19:06:40 +0000

    homework 2

--- 02-experiment-tracking/homework/HOMEWORK.md
+++ 02-experiment-tracking/homework/HOMEWORK.md
@@ -1,5 +1,28 @@
 # Homework 02 Experiment Tracking
 
-# Q2. Download and preprocess the data
+### Q1. Install MLflow
+```
+mlflow, version 2.17.0
+```
+### Q2. Download and preprocess the data
 
 How many files were saved to OUTPUT_FOLDER? 4
+
+### Q3. Train a model with autolog
+
+What is the value of the `min_samples_split` parameter: 2
+
+### Q4. Launch the tracking server locally
+
+```
+--default-artifact-root ./artifacts
+```
+
+### Q5. Tune model hyperparameters
+
+What's the best validation RMSE that you got?  5.335419588556921
+
+
+### Q6. Promote the best model to the model registry
+
+What is the test RMSE of the best model? 5.567408012462019
--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -10,7 +10,13 @@ from sklearn.metrics import mean_squared_error
 
 HPO_EXPERIMENT_NAME = "random-forest-hyperopt"
 EXPERIMENT_NAME = "random-forest-best-models"
-RF_PARAMS = ['max_depth', 'n_estimators', 'min_samples_split', 'min_samples_leaf', 'random_state']
+RF_PARAMS = [
+    "max_depth",
+    "n_estimators",
+    "min_samples_split",
+    "min_samples_leaf",
+    "random_state",
+]
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment(EXPERIMENT_NAME)
@@ -71,10 +77,19 @@ def run_register_model(data_path: str, top_n: int):
 
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
 
+    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.test_rmse ASC"]
+    )[0]
     # Register the best model
     # mlflow.register_model( ... )
+
+    run_id = best_run.info.run_id
+    mlflow.register_model(f"runs:/{run_id}/model", name="random-forest-best-model")
 
 
 if __name__ == '__main__':
